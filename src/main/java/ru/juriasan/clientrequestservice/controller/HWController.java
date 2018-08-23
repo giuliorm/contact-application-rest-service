60bee66269747e9db40c60e73e1df762e308a833
Iuliia Zotova <izotova@actumdigital.de>
2018-08-23 12:33:03 +0200

    hw correction

--- src/main/java/ru/juriasan/clientrequestservice/controller/HWController.java
+++ src/main/java/ru/juriasan/clientrequestservice/controller/HWController.java
@@ -1,13 +1,15 @@
 package ru.juriasan.clientrequestservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HWController {
 
     @GetMapping(value = "/")
     public String HW () {
-        return "HW";
+        return "Hello W!";
     }
 }
